86f419bc102b3e927e083edbb67439cee47077d3
Gaurav <gauravrajahela884@gmail.com>
2024-12-26 13:07:30 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import './globals.css'
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { Inter } from 'next/font/google'
+import NextTopLoader from "nextjs-toploader";
+
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'True Feedback',
+  description: 'Real feedback from real people.',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+          <NextTopLoader />
+          <Toaster />
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
